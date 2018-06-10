b89b45fc3f272bc3c7431abc974b271764ebf033
TheMetamorphics <themetamorphics@gmail.com>
2018-06-10 12:59:42 -0500

    Refactoring key detection

--- lua/autorun/server/keydetection.lua
+++ lua/autorun/server/keydetection.lua
@@ -5,38 +5,54 @@
 
 local antManWeapons = { "am_antman", "am_wasp", "am_yellowjacket", "am_shrinkgun", "am_growgun" }
 local antManSuits = { "am_antman", "am_wasp", "am_yellowjacket" }
+local shrinkMin = 0.11
+local shrinkMax = 0.9
+local shrinkIncrement = 0.1
+local growMin = 1.5
+local growMax = 5
+local growIncrement = 0.5
 
-hook.Add("PlayerButtonDown", "am_KeyDetection", function(ply, btn)
+hook.Add("PlayerButtonDown", "am_ActivationKey", function(ply, btn)
+
+  local wep = ply:GetActiveWeapon()
+
+  if !IsValid(wep) or !table.HasValue(antManSuits, wep:GetClass()) then return end
+
+  if btn == KEY_J then
+    if ply:GetModelScale() != 1 then ply:am_SetModelScale(1, 0.2) return end
+    if wep:GetSizeMode() == "Shrink" then
+      ply:am_SetModelScale(wep:GetShrinkTargetSize(), 0.2)
+    else
+      ply:am_SetModelScale(wep:GetGrowTargetSize(), 0.2)
+    end
+  end
+
+end)
+
+hook.Add("PlayerButtonDown", "am_DecreaseKey", function(ply, btn)
 
   local wep = ply:GetActiveWeapon()
 
   if !IsValid(wep) or !table.HasValue(antManWeapons, wep:GetClass()) then return end
 
-  if table.HasValue(antManSuits, wep:GetClass()) then
-    if btn == KEY_J then
-      if ply:GetModelScale() != 1 then ply:am_SetModelScale(1, 0.2) return end
-      if wep:GetSizeMode() == "Shrink" then
-        ply:am_SetModelScale(wep:GetShrinkTargetSize(), 0.2)
-      else
-        ply:am_SetModelScale(wep:GetGrowTargetSize(), 0.2)
-      end
-    end
+  if btn == KEY_N and wep:GetSizeMode() == "Shrink" and wep:GetShrinkTargetSize() > shrinkMin then
+    wep:SetShrinkTargetSize(wep:GetShrinkTargetSize() - shrinkIncrement)
+  elseif btn == KEY_N and wep:GetSizeMode() == "Grow" and wep:GetGrowTargetSize() > growMin then
+    wep:SetGrowTargetSize(wep:GetGrowTargetSize() - growIncrement)
   end
 
-  if table.HasValue(antManWeapons, wep:GetClass()) then
+end)
 
-    if btn == KEY_N and wep:GetSizeMode() == "Shrink" and wep:GetShrinkTargetSize() > 0.11 then
-      wep:SetShrinkTargetSize(wep:GetShrinkTargetSize() - 0.1)
-    elseif btn == KEY_N and wep:GetGrowTargetSize() > 1.5 then
-      wep:SetGrowTargetSize(wep:GetGrowTargetSize() - 0.5)
-    end
+hook.Add("PlayerButtonDown", "am_IncreaseKey", function(ply, btn)
 
-    if btn == KEY_M and wep:GetSizeMode() == "Shrink" and wep:GetShrinkTargetSize() < 0.9 then
-      wep:SetShrinkTargetSize(wep:GetShrinkTargetSize() + 0.1)
-    elseif btn == KEY_M and wep:GetGrowTargetSize() < 5 then
-      wep:SetGrowTargetSize(wep:GetGrowTargetSize() + 0.5)
-    end
+  local wep = ply:GetActiveWeapon()
 
+  if !IsValid(wep) or !table.HasValue(antManWeapons, wep:GetClass()) then return end
+
+  if btn == KEY_M and wep:GetSizeMode() == "Shrink" and wep:GetShrinkTargetSize() < shrinkMax then
+    wep:SetShrinkTargetSize(wep:GetShrinkTargetSize() + shrinkIncrement)
+  elseif btn == KEY_M and wep:GetSizeMode() == "Grow" and wep:GetGrowTargetSize() < growMax then
+    wep:SetGrowTargetSize(wep:GetGrowTargetSize() + growIncrement)
   end
 
 end)
